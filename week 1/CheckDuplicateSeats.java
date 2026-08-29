f34bba31912cf637d352b7db61d37e48dd66494b
sasidharun714-dotcom <sasidharun714@gmail.com>
2026-08-29 15:53:10 +0530

    Add duplicate seat checker

---
+++ week 1/CheckDuplicateSeats.java
@@ -0,0 +1,30 @@
+public class CheckDuplicateSeats {
+
+    public static void main(String[] args) {
+
+        int[] seatNumbers = {101, 102, 103, 102, 105};
+
+        checkDuplicateSeats(seatNumbers);
+    }
+
+    static void CheckDuplicateSeats(int[] seatNumbers) {
+
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+
+                if (seatNumbers[i] == seatNumbers[j]) {
+
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    duplicateFound = true;
+                }
+            }
+        }
+
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+}
